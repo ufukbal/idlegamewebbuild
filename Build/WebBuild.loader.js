9fb542320264cecd562f81601ddeabbaee74b625
Ufuk Bal <balufuk91@gmail.com>
2026-02-28 17:34:51 +0100

    new build

--- Build/WebBuild.loader.js
+++ Build/WebBuild.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 97252039,
+    wasmFileSize: 97257594,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
